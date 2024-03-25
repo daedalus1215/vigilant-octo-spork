5dbb88b507fb26ef26a639b02fad02f8d264011c
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2024-03-25 18:10:43 -0400

    wire-in-mongo-and-fetch: Wire up app and todo modules

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -4,24 +4,16 @@ import { AppService } from './app.service';
 import { TodoModule } from './todo/todo.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import { TodoCategory } from './todo/entity/todo-category.schema';
+import { TodoCategory, TodoCategorySchema } from './todo/entity/todo-category.schema';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
+    MongooseModule.forRoot('mongodb://localhost/test'),
+    MongooseModule.forFeature([{name: TodoCategory.name, schema: TodoCategorySchema}]),
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: `.env.${process.env.NODE_ENV}`,
-    }),
-    TypeOrmModule.forRootAsync({
-      inject: [ConfigService],
-      useFactory: (config: ConfigService) => {
-        return {
-          type: 'mongodb',
-          host: 'localhost',
-          synchronize: false,
-          entities: [TodoCategory]
-        }
-      }
     }),
     TodoModule
   ],
