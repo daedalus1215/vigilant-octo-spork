90fafc85496f681293111881510c89828a00b36b
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2024-03-25 18:10:17 -0400

    wire-in-mongo-and-fetch: Adjust service with the new interface

--- server/src/todo/todo.service.ts
+++ server/src/todo/todo.service.ts
@@ -1,21 +1,26 @@
 import { Injectable } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
-import { TodoCategory } from './entity/todo-category.schema';
-import { Repository } from 'typeorm';
+import { TodoCategory, TodoCategoryDocument } from './entity/todo-category.schema';
 import { CreateTodoCategoryDto } from './dtos/create-todo-category.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 
 @Injectable()
 export class TodoService {
-    constructor(@InjectRepository(TodoCategory) private todoRepository: Repository<TodoCategory>) { }
+    constructor(@InjectModel(TodoCategory.name) private model: Model<TodoCategoryDocument>) { }
+
+    async findAll() {
+        return await this.model.find();
+    }   
+
     async findOne(id: string) {
         if (!id) {
             return null;
         }
-        return await this.todoRepository.findOneBy({ id });
+        return await this.model.findById(id);
     }
 
-    create(dto: CreateTodoCategoryDto) {
-        const todo = this.todoRepository.create((dto as TodoCategory));
-        return this.todoRepository.save(todo);
+    async create(dto: CreateTodoCategoryDto) {
+        const todo = await this.model.create((dto as TodoCategory));
+        return todo.save();
     }
 }
